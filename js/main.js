7e777d3c1b49b33de885e9c662ef1151d5def9c2
nzr_pe <joseluis.von@gmail.com>
2022-02-23 03:34:23 -0500

    js y detalles

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//botones inferiores
+console.log("estamosaqui");
+let btnyo =document.getElementById("btnyo");   
+let btnlinkedin = document.getElementById("btnlinkedin");
+let btnproyectos = document.getElementById("btnproyectos");
+
+function mostrarsobremi(){
+   let sobremi = document.getElementById("descripcion");
+   let linkedin = document.getElementById("cardlinkedin");
+   let cardproyecto = document.getElementsByClassName("cardproyecto");
+   sobremi.style.display="flex";
+   linkedin.style.display="none";
+   cardproyecto[0].style.display="none";
+   cardproyecto[1].style.display="none";
+   cardproyecto[2].style.display="none";
+}
+
+function mostrarlinkedin(){
+    let linkedin = document.getElementById("cardlinkedin");
+    let sobremi = document.getElementById("descripcion");
+    let cardproyecto = document.getElementsByClassName("cardproyecto");
+    sobremi.style.display="none";
+    linkedin.style.display="flex";
+    cardproyecto[0].style.display="none";
+    cardproyecto[1].style.display="none";
+    cardproyecto[2].style.display="none";
+}
+
+function mostrarproyectos(){
+    let cardproyecto = document.getElementsByClassName("cardproyecto");
+    let linkedin = document.getElementById("cardlinkedin");
+    let sobremi = document.getElementById("descripcion");
+    sobremi.style.display="none";
+    linkedin.style.display="none";
+    cardproyecto[0].style.display="flex";
+    cardproyecto[1].style.display="flex";
+    cardproyecto[2].style.display="flex";
+}
+btnproyectos.addEventListener("click", mostrarproyectos);
+btnyo.addEventListener("click", mostrarsobremi);
+btnlinkedin.addEventListener("click", mostrarlinkedin);
