65e2ccdfcefbed200a591ef3ea4ffd94edb8ce08
nzr_pe <joseluis.von@gmail.com>
2022-04-01 04:05:51 -0500

    btnplux

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log("estamosaqui");
 let btnyo =document.getElementById("btnyo");   
 let btnlinkedin = document.getElementById("btnlinkedin");
 let btnproyectos = document.getElementById("btnproyectos");
-
+let masproyectos = document.getElementById("masproyectos");
 function mostrarsobremi(){
    let sobremi = document.getElementById("descripcion");
    let linkedin = document.getElementById("cardlinkedin");
@@ -13,6 +13,7 @@ function mostrarsobremi(){
    cardproyecto[0].style.display="none";
    cardproyecto[1].style.display="none";
    cardproyecto[2].style.display="none";
+   masproyectos.style.display="none";
 }
 
 function mostrarlinkedin(){
@@ -24,17 +25,21 @@ function mostrarlinkedin(){
     cardproyecto[0].style.display="none";
     cardproyecto[1].style.display="none";
     cardproyecto[2].style.display="none";
+    masproyectos.style.display="none";
 }
 
 function mostrarproyectos(){
     let cardproyecto = document.getElementsByClassName("cardproyecto");
     let linkedin = document.getElementById("cardlinkedin");
     let sobremi = document.getElementById("descripcion");
+   
+    
     sobremi.style.display="none";
     linkedin.style.display="none";
     cardproyecto[0].style.display="flex";
     cardproyecto[1].style.display="flex";
     cardproyecto[2].style.display="flex";
+    masproyectos.style.display="flex";
 }
 btnproyectos.addEventListener("click", mostrarproyectos);
 btnyo.addEventListener("click", mostrarsobremi);
