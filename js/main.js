ca79b30bd34c32b37ad3bd9b9a87e154a9b528c0
nzr_pe <joseluis.von@gmail.com>
2022-04-06 02:35:19 -0500

    masproyectos functions

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ let btnyo =document.getElementById("btnyo");
 let btnlinkedin = document.getElementById("btnlinkedin");
 let btnproyectos = document.getElementById("btnproyectos");
 let masproyectos = document.getElementById("masproyectos");
+
+let btnmasproyectos = document.getElementById("btnmasproyectos");
+let proyectos2 = document.getElementById("proyectos2");
+
 function mostrarsobremi(){
    let sobremi = document.getElementById("descripcion");
    let linkedin = document.getElementById("cardlinkedin");
@@ -14,6 +18,7 @@ function mostrarsobremi(){
    cardproyecto[1].style.display="none";
    cardproyecto[2].style.display="none";
    masproyectos.style.display="none";
+   proyectos2.style.display="none";
 }
 
 function mostrarlinkedin(){
@@ -26,6 +31,7 @@ function mostrarlinkedin(){
     cardproyecto[1].style.display="none";
     cardproyecto[2].style.display="none";
     masproyectos.style.display="none";
+    proyectos2.style.display="none";
 }
 
 function mostrarproyectos(){
@@ -40,7 +46,33 @@ function mostrarproyectos(){
     cardproyecto[1].style.display="flex";
     cardproyecto[2].style.display="flex";
     masproyectos.style.display="flex";
+    proyectos2.style.display="none";
 }
+
+function mostrarproyectossecundarios(){
+    let cardproyecto = document.getElementsByClassName("cardproyecto");
+    let linkedin = document.getElementById("cardlinkedin");
+    let sobremi = document.getElementById("descripcion");
+    let proyectos2 = document.getElementById("proyectos2");
+    let cardproyecto2 = document.getElementsByClassName("cardproyecto2");
+    
+    sobremi.style.display="none";
+    linkedin.style.display="none";
+    cardproyecto[0].style.display="none";
+    cardproyecto[1].style.display="none";
+    cardproyecto[2].style.display="none";
+    masproyectos.style.display="none";
+    proyectos2.style.display="flex";
+
+    cardproyecto2[0].style.display="flex";
+    cardproyecto2[1].style.display="flex";
+    cardproyecto2[2].style.display="flex";
+
+
+}
+
 btnproyectos.addEventListener("click", mostrarproyectos);
 btnyo.addEventListener("click", mostrarsobremi);
 btnlinkedin.addEventListener("click", mostrarlinkedin);
+
+btnmasproyectos.addEventListener("click", mostrarproyectossecundarios)
